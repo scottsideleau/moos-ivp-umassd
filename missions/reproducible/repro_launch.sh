56877c570ae77c39a3eec8efa51f0564bf4a7a6b
Scott R. Sideleau <scott@sideleau.com>
2016-12-18 11:47:52 -0500

    Refactored sleep schedule to be dependent on specific apps finishing.

--- missions/reproducible/repro_launch.sh
+++ missions/reproducible/repro_launch.sh
@@ -3,18 +3,27 @@
 # Start by launching the simulation environment
 echo "Starting the MOOS-IvP environment."
 source simulation_launch.sh
+sleep 1
 
 # Wait for the IvP Helm to start
+echo "Starting the MOOS-IvP mission."
 while [ $(pgrep pHelmIvP) ]; do
   sleep 1
 done
 
-# Start the mission (quietly)
-sleep 1
-echo "Starting the MOOS-IvP mission."
-source simulation_launch.sh
+# Engage the IvP Helm (quietly)
 source scripts/engage_ivphelm.sh >& /dev/null &
+while [ $(pgrep uPokeDB) ]; do
+  echo "Engaging the IvP Helm."
+  sleep 1
+done
+
+# Release control to the IvP Helm (quietly)
 source scripts/release_control.sh >& /dev/null &
+while [ $(pgrep uPokeDB) ]; do
+  echo "Releasing control to the IvP Helm."
+  sleep 1
+done
 
 # Check if mission is still running
 while [ $(pgrep pAntler) ]; do
@@ -24,4 +33,8 @@ done
 echo "Completed the MOOS-IvP mission."
 
 # Post-process the MOOS-IvP mission
+while [ $(pgrep pLogger) ]; do
+  echo "Waiting for log files to be closed..."
+  sleep 1
+done
 
