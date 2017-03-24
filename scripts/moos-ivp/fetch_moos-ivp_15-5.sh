d13bacd8f4bd4cd7b112e3d6365ebd9955205818
Scott R. Sideleau <scottsideleau@users.noreply.github.com>
2017-03-24 15:56:10 -0400

    Added support for "--robot" or "-r" command line switch.

--- scripts/moos-ivp/fetch_moos-ivp_15-5.sh
+++ scripts/moos-ivp/fetch_moos-ivp_15-5.sh
@@ -1,14 +1,34 @@
 #! /bin/bash
 
 NPROC=`getconf _NPROCESSORS_ONLN`
+ROBOT=false
+
+for ARGI; do
+  if [ "$ARGI" = "--robot" -o "$ARGI" = "-r" ]; then
+    ROBOT=true
+  fi
+done
 
 pushd /opt
+
 git clone https://github.com/moos-ivp/MOOS-IvP-releases.git && \
 mv MOOS-IvP-releases moos-ivp && \
 cd moos-ivp && \
 git checkout tags/15.5 && \
+sleep 1
+
 sed -i s/j12/j${NPROC}/ build-ivp.sh && \
 sed -i s/O3/Os/ build-ivp.sh && \
+sleep 1
+
+if [ "$ROBOT" = true ]; then
+  sed -i s/BUILD_GUI_CODE=\"ON\"/BUILD_GUI_CODE=\"OFF\"/ build-ivp.sh
+fi
+sleep 1
+
 sed -i s/j12/j${NPROC}/ build-moos.sh && \
+sleep 1
+
 ./build.sh
+
 popd
