9d448d23dcf44126a92663c2534540f443fc87c5
Scott R. Sideleau <scott@sideleau.com>
2017-05-01 23:05:00 -0400

    Wrapped quotes around if-variables to account for potentially being empty.

--- missions/reproducible/repro_launch.sh
+++ missions/reproducible/repro_launch.sh
@@ -5,7 +5,7 @@ source simulation_launch.sh
 echo "Starting the MOOS-IvP environment."
 
 # Wait for the IvP Helm to start
-until [ $(pgrep pHelmIvP) ]; do
+until [ "$(pgrep pHelmIvP)" ]; do
   echo "Waiting on the IvP Helm..."
   sleep 1
 done
@@ -13,25 +13,25 @@ done
 # Engage the IvP Helm (quietly)
 echo "Starting the MOOS-IvP mission."
 source scripts/engage_ivphelm.sh >& /dev/null &
-while [ $(pgrep uPokeDB) ]; do
+while [ "$(pgrep uPokeDB)" ]; do
   echo "Engaging the IvP Helm..."
   sleep 1
 done
 
 # Release control to the IvP Helm (quietly)
 source scripts/release_control.sh >& /dev/null &
-while [ $(pgrep uPokeDB) ]; do
+while [ "$(pgrep uPokeDB)" ]; do
   echo "Releasing control to the IvP Helm..."
   sleep 1
 done
 
 # Check if mission is still running
-while [ $(pgrep pAntler) ]; do
+while [ "$(pgrep pAntler)" ]; do
   echo "MOOS-IvP mission running..."
   sleep 1
 done
 echo "Completed the MOOS-IvP mission."
-while [ $(pgrep pLogger) ]; do
+while [ "$(pgrep pLogger)" ]; do
   echo "Waiting for log files to be closed..."
   sleep 1
 done
