65539e958cc9ffc5fcfd65ff20b68a61a1125df4
Scott R. Sideleau <scott@sideleau.com>
2017-05-08 17:28:29 -0400

    Hardened for use on MacOS.

--- scripts/moos-ivp/fetch_moos-ivp_15-5.sh
+++ scripts/moos-ivp/fetch_moos-ivp_15-5.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
 NPROC=`getconf _NPROCESSORS_ONLN`
+EXT=".bak"
 ROBOT=false
 
+# Handle command line options
 for ARGI; do
-  if [ "$ARGI" = "--robot" -o "$ARGI" = "-r" ]; then
+  if [ "'$ARGI' = '--robot' -o '$ARGI' = '-r'" ]; then
     ROBOT=true
   fi
 done
+
+# Setup the 'sed' command for in-place file changes
+if [ "$(uname) = 'Darwin'" ]; then
+  # MacOS
+  SED="sed -i $EXT"
+else
+  # Linux, etc.
+  SED="sed -i"
+fi
 
 pushd /opt
 
@@ -17,18 +28,25 @@ cd moos-ivp && \
 git checkout tags/15.5 && \
 sleep 1
 
-sed -i s/j12/j${NPROC}/ build-ivp.sh && \
-sed -i s/O3/Os/ build-ivp.sh && \
+$SED s/j12/j${NPROC}/ build-ivp.sh && \
+$SED s/O3/Os/ build-ivp.sh && \
 sleep 1
 
-if [ "$ROBOT" = true ]; then
-  sed -i s/BUILD_GUI_CODE=\"ON\"/BUILD_GUI_CODE=\"OFF\"/ build-ivp.sh
+if [ "'$ROBOT' = true" ]; then
+  $SED s/BUILD_GUI_CODE=\"ON\"/BUILD_GUI_CODE=\"OFF\"/ build-ivp.sh
 fi
 sleep 1
 
-sed -i s/j12/j${NPROC}/ build-moos.sh && \
+$SED s/j12/j${NPROC}/ build-moos.sh && \
 sleep 1
+
+# OS-specific clean-up
+if [ "$(uname) = 'Darwin'" ]; then
+  # MacOS
+  rm -f *$EXT
+fi
 
 ./build.sh
 
 popd
+
